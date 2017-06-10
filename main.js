5c2b4c8c28465eeacd6372091b9bbeeb2dcc9e04
quyenluu <quyenluu1989@gmail.com>
2017-06-10 18:54:24 +0700

    fix connect peer with host heroku

--- main.js
+++ main.js
@@ -41,7 +41,7 @@ function playStream(idVideoTag, stream){
 // openStream()
 // .then(stream => playStream("localStream", stream));
 
-const peer = new Peer({key: 'peerjs', host: 'https://serverpeerjs.herokuapp.com', secure: true, port: 443});
+const peer = new Peer({key: 'peerjs', host: 'serverpeerjs.herokuapp.com', secure: true, port: 443});
 
 peer.on("open", id => {
     $("#my-peer").append(id);
